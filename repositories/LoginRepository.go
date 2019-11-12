07844b5dcfa450f31d4ed48868e13a4410c9c54e
luthfi <mluthfi@jojonomic.com>
2019-11-12 09:15:05 +0700

    add UserModel

---
+++ repositories/LoginRepository.go
@@ -0,0 +1,47 @@
+package repositories
+
+import (
+	"fmt"
+	"github.com/afex/hystrix-go/hystrix"
+	"github.com/djamboe/mtools-login-service/interfaces"
+	"github.com/djamboe/mtools-login-service/models"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+type LoginRepositoryWithCircuitBreaker struct {
+	LoginRepository interfaces.ILoginRepository
+}
+
+func (repository *LoginRepositoryWithCircuitBreaker) DoLogin(username string, password string) (models.UserModel, error) {
+	output := make(chan models.UserModel, 1)
+	hystrix.ConfigureCommand("get_user_by_username_and_password", hystrix.CommandConfig{Timeout: 1000})
+	errors := hystrix.Go("get_user_by_username_and_password", func() error {
+		user, _ := repository.LoginRepository.GetUserByEmailAndPassword(username, password)
+		output <- user
+		return nil
+	}, nil)
+
+	select {
+	case out := <-output:
+		return out, nil
+	case err := <-errors:
+		println(err)
+		return models.UserModel{}, err
+	}
+}
+
+type LoginRepository struct {
+	interfaces.IDbHandler
+}
+
+func (repository *LoginRepository) DoLogin(username string, password string) (models.UserModel, error) {
+	row, err := repository.Query(fmt.Sprintf("SELECT * FROM user_model WHERE username = '%s' AND password = '%s'", username, password))
+	if err != nil {
+		return models.UserModel{}, err
+	}
+
+	var user models.UserModel
+	row.Next()
+	row.Scan(&user.Id, &user.Name)
+	return user, nil
+}
---
+++ viewmodels/UserVM.go
@@ -0,0 +1,7 @@
+package viewmodels
+
+import "github.com/djamboe/mtools-login-service/models"
+
+type UserVM struct {
+	User models.UserModel
+}
