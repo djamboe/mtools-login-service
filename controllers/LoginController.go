6ebd432befeb38ce5daaf09c60d1824446197d7f
luthfi <mluthfi@jojonomic.com>
2019-11-12 09:58:53 +0700

    add UserLoginModelParam

--- controllers/LoginController.go
+++ controllers/LoginController.go
@@ -1,9 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/djamboe/mtools-login-service/interfaces"
+	"github.com/djamboe/mtools-login-service/models"
+	"github.com/go-chi/render"
 )
 
 type LoginController struct {
@@ -11,5 +15,14 @@ type LoginController struct {
 }
 
 func (controller *LoginController) LoginProcess(res http.ResponseWriter, req *http.Request) {
-
+	var params models.UserLoginParamModel
+	s, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(s, &params)
+	if err != nil {
+		panic(err)
+	}
+	render.JSON(res, req, params)
 }
