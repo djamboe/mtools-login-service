8d0567c68814c0dfb7e835276dd97aa90671fcbb
luthfi <mluthfi@jojonomic.com>
2019-12-01 08:25:33 +0700

    add grpc gateway and protobuff

--- controllers/LoginController.go
+++ controllers/LoginController.go
@@ -1,36 +1,16 @@
 package controllers
 
 import (
-	"encoding/json"
-	"github.com/djamboe/mtools-login-service/models"
-	"github.com/djamboe/mtools-login-service/viewmodels"
-	"github.com/go-chi/render"
-	"io/ioutil"
-	"net/http"
-
 	"github.com/djamboe/mtools-login-service/interfaces"
+	"github.com/djamboe/mtools-login-service/viewmodels"
 )
 
 type LoginController struct {
 	interfaces.ILoginService
 }
 
-func (controller *LoginController) LoginProcess(res http.ResponseWriter, req *http.Request) {
-	var user models.UserLoginParamModel
+func (controller *LoginController) LoginProcess(username string, password string) (viewmodels.LoginVM, error) {
 	var response viewmodels.LoginVM
-
-	s, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	err = json.Unmarshal(s, &user)
-	if err != nil {
-		panic(err)
-	}
-
-	username := user.Username
-	password := user.Password
 
 	login, err := controller.DoLogin(username, password)
 	if err != nil {
@@ -41,12 +21,12 @@ func (controller *LoginController) LoginProcess(res http.ResponseWriter, req *ht
 		response.Error = false
 		response.Id = login.Id
 		response.Username = login.Username
-	}else{
+	} else {
 		response.Error = true
 		response.Id = login.Id
 		response.Username = login.Username
 	}
 
-	render.JSON(res, req,response)
+	return response, nil
 
 }
